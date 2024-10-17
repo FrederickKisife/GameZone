c5c4e79db23a28316e6b9175ac0a46f7201197a4
FrederickKisife <kilafrederick@gmail.com>
2024-10-17 04:15:54 -0700

    Returning errors to the user if form fields are not validated

--- styles/global.js
+++ styles/global.js
@@ -23,6 +23,13 @@ export const globalStyles = StyleSheet.create({
         padding: 15,
         borderRadius: 6,
 
+    },
+    errorText:{
+        color:"crimson",
+        fontWeight:"bold",
+        marginBottom:10,
+        marginTop:6,
+        textAlign:"center"
     }
 })
 
